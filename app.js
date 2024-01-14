5d5989cdfeba91a474f72580be0351cc1ea70d63
DOMAA21 <clashecurry21@gmail.com>
2024-01-15 01:59:32 +0800

    enable cors to client

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(cors({
   // origin: 'http://localhost:3000', 
-  origin: ['http://localhost:3000','http://localhost:3001'], 
+  origin: ['http://localhost:3000','http://localhost:3001','https://treatsdelight.vercel.app'], 
   credentials: true
 }));
 
