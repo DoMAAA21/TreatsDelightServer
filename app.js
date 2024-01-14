0c0ad34035b83fe1c530ea601798b3b00e8f2ec8
DOMAA21 <clashecurry21@gmail.com>
2024-01-15 02:47:50 +0800

    conf cors

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(cors({
   // origin: 'https://treatsdelight.vercel.app', 
-  origin: ['http://localhost:3000','http://localhost:3001','https://treatsdelight.vercel.app'], 
+  origin: ['http://localhost:3000','https://treatsdelight.vercel.app'], 
   credentials: true
 }));
 
