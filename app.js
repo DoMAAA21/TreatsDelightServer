7374c31ac1f974deb26a01ffd6d17f2649cb87dc
DOMAA21 <clashecurry21@gmail.com>
2024-01-15 09:10:25 +0800

    render update

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(cors({
   // origin: 'https://treatsdelight.vercel.app', 
-  origin: ['http://localhost:3000','https://treatsdelight.vercel.app'], 
+  origin: ['http://localhost:3000','https://treatsdelight.vercel.app','https://treats-delight.onrender.com'], 
   credentials: true
 }));
 
