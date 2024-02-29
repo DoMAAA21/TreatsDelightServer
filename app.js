a4c9cf768bf4263fdccd0f2a760cb14c252000b0
Jharold Cataluña <128008589+DoMAAA21@users.noreply.github.com>
2024-02-29 11:01:34 +0800

    cors updated

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-  origin: 'https://treatsdelight.vercel.app/', 
+  origin: 'https://treatsdelight.vercel.app', 
   // origin: ['http://localhost:3000','http://localhost:3001'], 
   credentials: true
 }));
